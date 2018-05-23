3c16fd3553c651e04186f0b98835d994ba8433eb
Josh Stegmeier <josh@steg.me>
2018-05-23 20:19:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,17 @@ class Song
     @artist = artist
     @genre = genre
 
+    unless SONGNAMES.include?(name)
+      SONGNAMES << name
+    end
+
+    unless ARTISTS.include?(artist)
+      ARTISTS << artist
+    end
+
+    unless GENRES.include?(genre)
+      GENRES >> genre
+    end
 
   end
 
