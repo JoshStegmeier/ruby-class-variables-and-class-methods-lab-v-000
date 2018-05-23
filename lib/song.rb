fa47b25b2db1052bb9b321135edbaf022889a032
Josh Stegmeier <josh@steg.me>
2018-05-23 20:12:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,14 @@
 class Song
 
+  attr_accessor :name, :artist, :genre
+
   def self.new(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
+
+
 
   def name
     @name
